05c8a8f9c50e5936ba86629739ca95678199cccb
22wh1a12a4 <120735549+22wh1a12a4@users.noreply.github.com>
2023-01-30 19:12:18 +0530

    Add files via upload

---
+++ experiment 4 cycle 1/simple and compound interests.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+int p,t,r,si,ci;
+printf("enter the p,t,r");
+scanf("%d%d%d",&p,&t,&r);
+ si=p*t*r/100;
+ ci=p*pow(1+0/100,t);
+printf("the si is %d\n",si);
+printf("the ci is %d\n",ci);
+return 0;
+}
+//akulla mudunuri would like to go on a date with you so w
